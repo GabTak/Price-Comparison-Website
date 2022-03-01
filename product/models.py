38dbeca3afd2ae4ff0966a98200c8b0fa4d0552c
Gabriel <19704223@students.lincoln.ac.uk>
2022-03-01 23:48:55 +0000

    Added Models

--- product/models.py
+++ product/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null = True, blank = True)    
+    name = models.CharField(max_length=200, null = True)
+
+    def __str__(self):
+        return self.name
+
+
 
 
 class Product(models.Model):
@@ -7,3 +18,12 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     store = models.CharField(max_length=30)
     image = models.CharField(max_length=5000, null = True, blank = True)
+
+    def __str__(self):
+        return self.name
+
+    
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null = True, blank = True)
+    
